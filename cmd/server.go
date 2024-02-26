94316c217d8bb00d18f74ca3d5ff8b4179f71d22
TheDonDope <thedondope@hey.com>
2024-02-26 20:05:57 +0100

    feat: setup echo server

--- cmd/server.go
+++ cmd/server.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
 	fmt.Println("Welcome to Wits!")
+
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Serve static assets
+	e.Static("/assets", "assets")
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3000"))
 }
