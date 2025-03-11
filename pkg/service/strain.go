a740f7b3d087d617172c1c35ba86f72b2c4bf111
Christian Dobert <thedondope@hey.com>
2025-03-11 13:10:19 +0100

    feat(pkg/service): add strain service

---
+++ pkg/service/strain.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	can "github.com/TheDonDope/wits/pkg/cannabis"
+	"github.com/TheDonDope/wits/pkg/storage"
+)
+
+// StrainService provides operations on strains.
+type StrainService struct {
+	store *storage.StrainStore
+}
+
+// NewStrainService creates a new service layer for strains.
+func NewStrainService(store *storage.StrainStore) *StrainService {
+	return &StrainService{store: store}
+}
+
+// AddStrain adds a strain to the store.
+func (s *StrainService) AddStrain(strain *can.Strain) {
+	s.store.AddStrain(strain)
+}
+
+// GetAllStrains retrieves all strains from the store.
+func (s *StrainService) GetAllStrains() []*can.Strain {
+	return s.store.GetStrains()
+}
+
+// FindStrainByCultivar looks up a strain by its cultivar.
+func (s *StrainService) FindStrainByCultivar(cultivar string) *can.Strain {
+	return s.store.FindStrain(cultivar)
+}
